37c46a97cbb54328f3a82ee1012d5fc553ce25c6
Avishay Bassa <avishay@geekmedia.co.il>
2015-01-06 22:01:51 +0200

    accident

--- status.php
+++
@@ -1,9 +0,0 @@
-<?php
-$parse_uri = explode( 'wp-content', $_SERVER['SCRIPT_FILENAME'] );
-include( $parse_uri[0] . 'wp-load.php' );
-
-$post_id = $_GET['id'];
-$status = get_post_status($post_id);
-
-echo "The status of the post " . $post_id . " is: " . $status;
-?>
